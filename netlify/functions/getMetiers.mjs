63a08bfd4bef176dd839cc37500eabcdd4c2a1c1
Benny76340 <bdargent088@gmail.com>
2025-08-01 13:38:17 +0200

    Mise à jour getMetiers.mjs avec les variables sans VITE_

--- netlify/functions/getMetiers.mjs
+++ netlify/functions/getMetiers.mjs
@@ -6,19 +6,19 @@ export async function handler(event, context) {
       `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Métiers`,
       {
         headers: {
-          Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
-        }
+          Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+        },
       }
     );
 
     return {
       statusCode: 200,
-      body: JSON.stringify(response.data.records)
+      body: JSON.stringify(response.data.records),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Erreur lors de la récupération Airtable." })
+      body: JSON.stringify({ error: "Erreur lors de la récupération Airtable." }),
     };
   }
 }
